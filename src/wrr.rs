77e15367ad99d5a7b727a911da2768b7165a61a5
shonenada <shonenada@gmail.com>
2019-12-04 21:36:22 +0800

    feat(wrr): Add `fail` impl, add testcases for WeightedRoundRobinBalancer

--- src/wrr.rs
+++ src/wrr.rs
@@ -1,25 +1,43 @@
+use std::cmp;
+
 #[derive(Debug)]
 pub struct Server {
     weight: i32,
     effect_weight: i32,
     cur_weight: i32,
-    pub url: String,
+    url: String,
 }
 
 impl Server {
     pub fn new(url: String, weight: i32) -> Server {
         return Server {
-            effect_weight: weight,
+            effect_weight: 1,
             cur_weight: 0,
             weight,
             url,
         };
     }
+
+    pub fn get_weight(&self) -> i32 {
+        self.weight
+    }
+
+    pub fn get_effect_weight(&self) -> i32 {
+        self.effect_weight
+    }
+
+    pub fn get_cur_weight(&self) -> i32 {
+        self.cur_weight
+    }
+
+    pub fn get_url(&self) -> &String {
+        &self.url
+    }
 }
 
 #[derive(Debug)]
 pub struct WeightedRoundRobinBalancer {
-    pub servers: Vec<Server>,
+    servers: Vec<Server>,
 }
 
 impl WeightedRoundRobinBalancer {
@@ -27,8 +45,27 @@ impl WeightedRoundRobinBalancer {
         return WeightedRoundRobinBalancer { servers: vec![] };
     }
 
+    pub fn get_servers(&self) -> &Vec<Server> {
+        &self.servers
+    }
+
+    pub fn get_servers_mut(&mut self) -> &mut Vec<Server> {
+        &mut self.servers
+    }
+
+    pub fn search_server_by_url(&self, url: &String) -> Option<&Server> {
+        for each in self.get_servers() {
+            if each.get_url() == url {
+                return Some(each);
+            }
+        }
+        None
+    }
+
     pub fn insert_server(&mut self, server: Server) {
-        self.servers.push(server);
+        if let None = self.search_server_by_url(server.get_url()) {
+            self.servers.push(server);
+        }
     }
 
     pub fn insert_url(&mut self, url: String, weight: i32) {
@@ -53,9 +90,90 @@ impl WeightedRoundRobinBalancer {
                 best_weight = each.cur_weight;
             }
         }
-        let mut best = self.servers.get_mut(best_idx).unwrap();
-        best.cur_weight = best.cur_weight - total;
+        if let Some(mut best) = self.servers.get_mut(best_idx) {
+            best.cur_weight = best.cur_weight - total;
+            Some(best)
+        } else {
+            None
+        }
+    }
 
-        Some(best)
+    pub fn fail(&mut self, url: &String) {
+        for mut each in self.get_servers_mut() {
+            if each.get_url() == url {
+                let diff = cmp::max(each.weight / 3, 1);
+                each.effect_weight -= diff;
+                if each.effect_weight < 0  {
+                    each.effect_weight = 0
+                }
+
+                debug!("Failed server: {}, effect_weight: {}, diff: {}", url, each.effect_weight, diff);
+            }
+        }
+    }
+}
+
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_next() {
+        let url = "http://localhost:8081".to_string();
+
+        let mut wrr = WeightedRoundRobinBalancer::new();
+        wrr.insert_url(url.clone(), 5);
+
+        let r1 = wrr.next().unwrap();
+        assert!(r1.url == url.clone());
+        assert!(r1.effect_weight == 2);
+
+        let r2 = wrr.next().unwrap();
+        assert!(r2.effect_weight == 3);
+
+        let _r3 = wrr.next().unwrap(); // effect_weight = 4
+        let _r4 = wrr.next().unwrap(); // effect_weight = 5
+
+        let r5 = wrr.next().unwrap(); // effect_weight = max_weight
+        assert!(r5.effect_weight == 5);
+    }
+
+    #[test]
+    fn test_fail() {
+        let url = "http://localhost:8081".to_string();
+
+        let mut wrr = WeightedRoundRobinBalancer::new();
+        wrr.insert_url(url.clone(), 5);
+
+        let r1 = wrr.next().unwrap();
+        assert!(r1.url == url.clone());
+        assert!(r1.effect_weight == 2);
+
+        wrr.fail(&url);
+        let s1 = wrr.search_server_by_url(&url).unwrap(); // effect_weight = 1
+        assert!(s1.effect_weight == 1);
+
+        wrr.fail(&url);
+        let _s2 = wrr.search_server_by_url(&url).unwrap(); // effect_weight = 0
+        let s3 = wrr.search_server_by_url(&url).unwrap(); // still effect_weight = 0
+        assert!(s3.effect_weight == 0);
+    }
+
+    #[test]
+    fn test_fail_then_next() {
+        let url = "http://localhost:8086".to_string();
+
+        let mut wrr = WeightedRoundRobinBalancer::new();
+        wrr.insert_url(url.clone(), 3);
+
+        let r1 = wrr.next().unwrap();
+        assert!(r1.url == url.clone());
+        assert!(r1.effect_weight == 2);
+
+        wrr.fail(&url);
+        let s1 = wrr.search_server_by_url(&url).unwrap(); // effect_weight = 1
+        assert!(s1.effect_weight == 1);
+
+        let r2 = wrr.next().unwrap();
+        assert!(r2.effect_weight == 2);
     }
 }
