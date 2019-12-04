9736178cd8ee0fe44074808c176b6e3d9f106b25
shonenada <shonenada@gmail.com>
2019-12-04 22:10:39 +0800

    doc(README): Add README.md, add simple example

--- examples/simple.rs
+++ examples/simple.rs
@@ -1,17 +1,14 @@
-# Roundrobin
+use roundrobin::wrr::{Server, WeightedRoundRobinBalancer};
 
-A weighted roundrobin implementation.
-
-## Quick Start
-
-```rust
 fn main() {
     let url01 = "http://localhost:8081".to_string();
     let url02 = "http://localhost:8082".to_string();
     let server01 = Server::new(url01.clone(), 1);
     let mut rr = WeightedRoundRobinBalancer::new();
     rr.insert_server(server01); // default weight 1
-    rr.insert_url(url02.clone(), 2);
-    println!("Server: {}", rr.next().unwrap());
+    rr.insert_url(url02.clone(), 5);
+
+    for _i in 0..5 {
+        println!("{:?}", rr.next().unwrap());
+    }
 }
-```
